8474026fcf2c2ae8523e03c93c59893d7a83392b
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2024-04-14 14:39:33 +0300

    citydetail link

--- src/components/cityDetailCard/CityDetailCard.jsx
+++ src/components/cityDetailCard/CityDetailCard.jsx
@@ -1,9 +1,15 @@
-import './cityDetailCard.css'
+import "./cityDetailCard.css";
+import { Link } from 'react-router-dom';
 
 const CityDetailCard = () => {
   return (
-    <div>CityDetailCard</div>
-  )
-}
+    <center className="cityDetailCard">
+      {" "}
+      <Link to={`/citydetails/${id}`} className="city-name-container">
+        <h3 className="city-card-name">{name}</h3>
+      </Link>
+    </center>
+  );
+};
 
-export default CityDetailCard
+export default CityDetailCard;
